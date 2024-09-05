5da6b5af9e7d0353325a74c49eca2c778325f342
Joshua <joshuakruger66@live.com>
2024-09-05 23:01:35 +0200

    Added the background image, managed the layout for the different elements and added the crypto API to show the info for the selected coin.

---
+++ index.js
@@ -0,0 +1,29 @@
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.full})`
+        document.getElementById("author").textContent = data.user.name
+    })
+    .catch(err => {
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
+    })
+
+fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong")
+        }
+        return res.json()
+    })
+    .then(data => {
+        document.getElementById("crypto-top").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `
+        document.getElementById("crypto").innerHTML = `
+            <p>🚀: R${data.market_data.current_price.zar}</p>
+            <p>👆: R${data.market_data.high_24h.zar}</p>
+            <p>👇: R${data.market_data.low_24h.zar}</p>
+        `
+    })
+    .catch(err => console.log(err))
