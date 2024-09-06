fc7a2e491614f2c34e01f7f9303baaa96f46c655
Joshua <joshuakruger66@live.com>
2024-09-06 22:30:44 +0200

    Finished the extension to display crypto value, display current time, user's location weather and  the author of the photo that is used in the  background.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Fetch to get a random background image
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
@@ -8,6 +9,7 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
     })
 
+// Fetch accessing crypto data to show the value of certain crypto coins
 fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
     .then(res => {
         if (!res.ok) {
@@ -27,3 +29,31 @@ fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
         `
     })
     .catch(err => console.log(err))
+
+// Function to display current time on the extension
+function currentTime() {
+    const date = new Date()
+    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
+}
+setInterval(currentTime, 1000)
+
+// Getting user location for weather data
+navigator.geolocation.getCurrentPosition((position) => {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Weather data not available")
+        }
+        return res.json()
+    })
+    .then(data => {
+        const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />
+                <p class="weather-temp">${Math.round(data.main.temp)}</p>
+                <p class="weather-city">${data.name}<p>
+            `
+    })
+    .catch(err => console.error(err))
+  });
+
